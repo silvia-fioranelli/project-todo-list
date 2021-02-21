76e3fa0da4032c54f419cc320b584b7d596466be
Silvia Fioranelli <silvia@example.com>
2021-02-21 19:45:22 +0000

    feat(list): create simple todo list
    The list is hardcoded. Will be integrated with API later.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const todos = [
+    'todo text 1',
+    'todo text 2',
+    'todo text 3',
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>TODO List</h1>
+      <ol>
+        {todos.map((todo, i) => {
+          return (
+            <li key={`todo-${i}`}>
+              {todo}
+            </li>
+          )
+        })}
+      </ol>
     </div>
   );
 }
