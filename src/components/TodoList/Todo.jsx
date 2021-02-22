b369760d1f06f72b445d33158a917c028ed0a5c5
Silvia Fioranelli <silvia@example.com>
2021-02-22 17:01:42 +0000

    style(components): add bootstrap style

--- src/components/TodoList/Todo.jsx
+++ src/components/TodoList/Todo.jsx
@@ -10,9 +10,25 @@ export const Todo = (props) => {
 
   return (
     <div>
-      <button onClick={() => deleteTodo(item.id)}>Delete</button>
-      <button onClick={() => toggleTodo(item.id)}>Toggle</button>
-      <p>{item.content}</p>
+      <button
+        className="btn btn-danger btn-sm mt-1 mr-1"
+        onClick={() => deleteTodo(item.id)}
+      >
+        Delete
+      </button>
+      <button
+        className="btn btn-success btn-sm mt-1 mr-3"
+        onClick={() => toggleTodo(item.id)}
+      >
+        Toggle
+      </button>
+      <span
+        style={{
+          textDecoration: item.completed ? 'line-through' : '',
+        }}
+      >
+        {item.content}
+      </span>
     </div>
   );
 };
